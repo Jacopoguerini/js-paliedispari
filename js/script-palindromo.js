6e8b541c0f865cb7195de33b5e8f762779b9a171
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-05-03 15:03:52 +0200

    Bozza 1 esercizio 1

---
+++ js/script-palindromo.js
@@ -0,0 +1,36 @@
+// 1 - Palindroma
+
+// Chiedere all'utente di inserire una parola
+var word = prompt("Inserisci qui una parola");
+console.log(word);
+
+// lettere divise in array
+var arrayWord = word.split("");
+console.log(arrayWord);
+
+// lettere in ordine contrario
+var arrayReverse = arrayWord.reverse();
+console.log(arrayReverse);
+
+// parola al contrario
+var wordReverse = arrayReverse.join("");
+console.log(wordReverse);
+
+// Creare una funzione per capire se la parola inserita è palindroma
+
+function palindroma (word, wordReverse) {
+
+    if (word == wordReverse) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+var risultato = palindroma(word, wordReverse);
+
+if (risultato == true) {
+    alert("Dai dai dai la parola " + word +" è palindroma");
+} else {
+    alert("La parola " + word + " non è palindroma");
+}
